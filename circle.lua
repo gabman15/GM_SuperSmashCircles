74f070114c70e7ad7f83b9abc3d0bc9ea8388da7
gabman15 <gabrielmcninch@gmail.com>
2018-07-05 12:30:35 -0400

    added types

--- circle.lua
+++ circle.lua
@@ -2,7 +2,7 @@
 
 Circle = Class{}
 
-function Circle:init(x, y, width, height, type)
+function Circle:init(x, y, width, height)
     self.x = x
     self.y = y
     self.width = width
@@ -14,9 +14,17 @@ function Circle:init(x, y, width, height, type)
         BETA
         probably different names for characters and there will be more characters
     ]]
-    self.type = type
+    -- Default type is 0 (Red)
+    self.type = 0
+
+    -- Lives of the character (default starts at 5)
+    self.lives = 5
 
     -- Speed of Circle
     self.dy = 0
     self.dx = 0
+end
+
+function Circle:setType(type)
+    self.type = type
 end
