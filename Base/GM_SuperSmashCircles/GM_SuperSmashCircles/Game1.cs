6e0ab6202a0880ec53e607fdea8f40f53a98ac9a
regenerativep <regeneralp@gmail.com>
2018-07-17 20:11:58 -0400

    basic control methods

--- Base/GM_SuperSmashCircles/GM_SuperSmashCircles/Game1.cs
+++ Base/GM_SuperSmashCircles/GM_SuperSmashCircles/Game1.cs
@@ -20,10 +20,13 @@ namespace GM_SuperSmashCircles
         //DllImport(NativeLibName, CallingConvention= CallingConvention.Cdecl);
         //public static extern int SDL_GameControllerAddMapping(string mappingString);
         public List<Entity> Entities { get; set; }
-        //public List<User> Users { get; set; }
+        public User[] Users { get; set; }
         public List<Platform> Platforms { get; set; }
         public Gamemode CurrentGamemode { get; set; }
 
+        public EventEmitter Events { get; set; }
+
+        public int MaxUsers { get; set; }
         public double XGravity { get; set; }
         public double YGravity { get; set; }
         public double CollisionPrecision { get; set; }
@@ -33,6 +36,8 @@ namespace GM_SuperSmashCircles
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             //Debug.WriteLine(Joystick.GetState(1).IsConnected);
+
+            Events = new EventEmitter();
 
         }
 
@@ -44,24 +49,19 @@ namespace GM_SuperSmashCircles
         /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-            /*
-            Entity testentity = Entity.LoadFromFile("testentity.lua");
-            testentity.OnCollision.Call();
-            testentity.OnUpdate.Call();
-            Console.WriteLine("{0} {1} {2} {3} {4}", testentity.X, testentity.Y, testentity.Width, testentity.Height, testentity.RepelAmount);*/
-            //Gamemode gamemode = Gamemode.LoadFromFile("testgamemode.lua");
-
+            MaxUsers = 4;
             XGravity = 0;
             YGravity = 0;
             CollisionPrecision = 1;
 
             firstStep = false;
-
+            Users = new User[MaxUsers];
             Entities = new List<Entity>();
             Platforms = new List<Platform>();
             CurrentGamemode = Gamemode.LoadFromFile("gamemode_default.lua", this);
             base.Initialize();
+
+            Events.Emit("initialize");
         }
 
         /// <summary>
@@ -74,7 +74,8 @@ namespace GM_SuperSmashCircles
             spriteBatch = new SpriteBatch(GraphicsDevice);
             ContentManager.AddContent(Content.Load<Texture2D>("circle"), "circle");
             ContentManager.AddContent(Content.Load<Texture2D>("platform"), "platform");
-            // TODO: use this.Content to load your game content here
+
+            Events.Emit("content");
         }
 
         /// <summary>
@@ -143,6 +144,8 @@ namespace GM_SuperSmashCircles
             CurrentGamemode.OnUpdate?.Call();
 
             base.Update(gameTime);
+
+            Events.Emit("update");
         }
 
         /// <summary>
@@ -165,6 +168,7 @@ namespace GM_SuperSmashCircles
             spriteBatch.End();
 
             base.Draw(gameTime);
+            //Events.Emit("draw");
         }
         public bool RectangleInRectangle(double ax1, double ay1, double ax2, double ay2, double bx1, double by1, double bx2, double by2)
         {
---
+++ Base/GM_SuperSmashCircles/GM_SuperSmashCircles/User.cs
@@ -0,0 +1,166 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using NLua;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GM_SuperSmashCircles
+{
+    public class User
+    {
+        public int Number { get; set; }
+        public UserInputType InputType { get; set; }
+        public InputModule Input { get; set; }
+        public Entity Player { get; set; }
+        public User(int number, InputModule input)
+        {
+            Number = number;
+            Input = input;
+        }
+        
+    }
+    public abstract class InputModule
+    {
+        public bool GetUp()
+        {
+            return Get("up");
+        }
+        public bool GetDown()
+        {
+            return Get("down");
+        }
+        public bool GetLeft()
+        {
+            return Get("left");
+        }
+        public bool GetRight()
+        {
+            return Get("right");
+        }
+        public bool GetJump()
+        {
+            return Get("jump");
+        }
+        public bool GetDash()
+        {
+            return Get("dash");
+        }
+        public bool GetSpecial()
+        {
+            return Get("special");
+        }
+        public abstract bool Get(string name);
+    }
+    public class LeftKeyboardInputModule : KeyboardInputModule
+    {
+        public LeftKeyboardInputModule(Game1 game) : base(game)
+        {
+            KeyboardControls = new Dictionary<string, Keys>();
+            KeyboardControls.Add("jump", Keys.Space);
+            KeyboardControls.Add("up", Keys.W);
+            KeyboardControls.Add("down", Keys.S);
+            KeyboardControls.Add("left", Keys.A);
+            KeyboardControls.Add("right", Keys.D);
+            KeyboardControls.Add("dash", Keys.H);
+            KeyboardControls.Add("special", Keys.J);
+        }
+    }
+    public class RightKeyboardInputModule : KeyboardInputModule
+    {
+        public RightKeyboardInputModule(Game1 game) : base(game)
+        {
+            KeyboardControls = new Dictionary<string, Keys>();
+            KeyboardControls.Add("jump", Keys.Multiply);
+            KeyboardControls.Add("up", Keys.Up);
+            KeyboardControls.Add("down", Keys.Down);
+            KeyboardControls.Add("left", Keys.Left);
+            KeyboardControls.Add("right", Keys.Right);
+            KeyboardControls.Add("dash", Keys.Add);
+            KeyboardControls.Add("special", Keys.Subtract);
+        }
+    }
+    public abstract class KeyboardInputModule : InputModule
+    {
+        private Game1 game;
+        private KeyboardState keyState;
+        public Dictionary<string, Keys> KeyboardControls { get; set; }
+        public KeyboardInputModule(Game1 game)
+        {
+            this.game = game;
+
+            game.Events.On("update", () =>
+            {
+                keyState = Keyboard.GetState();
+            });
+        }
+        public override bool Get(string name)
+        {
+            return keyState.IsKeyDown(KeyboardControls[name]);
+        }
+    }
+    public class XBOneInputModule : InputModule
+    {
+        private Game1 game;
+        private GamePadState gamepadState;
+        public Dictionary<string, Buttons> GamepadControls { get; set; }
+        public PlayerIndex Player { get; set; }
+        public XBOneInputModule(Game1 game, PlayerIndex player)
+        {
+            this.game = game;
+            Player = player;
+            GamepadControls = new Dictionary<string, Buttons>();
+            GamepadControls.Add("jump", Buttons.X);
+            GamepadControls.Add("up", Buttons.DPadUp);
+            GamepadControls.Add("down", Buttons.DPadDown);
+            GamepadControls.Add("left", Buttons.DPadLeft);
+            GamepadControls.Add("right", Buttons.DPadRight);
+            GamepadControls.Add("dash", Buttons.A);
+            GamepadControls.Add("special", Buttons.B);
+
+            game.Events.On("update", () =>
+            {
+                gamepadState = GamePad.GetState(Player);
+            });
+        }
+        public override bool Get(string name)
+        {
+            return gamepadState.IsButtonDown(GamepadControls[name]);
+        }
+    }
+    public class LuaInputModule : InputModule
+    {
+        private Game1 game;
+        public Lua State { get; set; }
+        public LuaInputModule(Game1 game, string filename)
+        {
+            this.game = game;
+            State = new Lua();
+            State.LoadCLRPackage();
+            State.DoFile(filename);
+            State.GetFunction("OnCreation")?.Call();
+            game.Events.On("update", () =>
+            {
+                State.GetFunction("OnUpdate")?.Call();
+            });
+        }
+        public override bool Get(string name)
+        {
+            try
+            {
+                bool result = (bool)State.GetFunction("GetUp")?.Call()[0];
+            }
+            catch (InvalidCastException)
+            {
+                return false;
+            }
+            return false;
+        }
+    }
+    public enum UserInputType
+    {
+        KeyboardLeft, KeyboardRight, XBOne, Lua
+    }
+}
